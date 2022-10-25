2e6fffc51102ff88f6c8cb833a2f1058d1ac3c1a
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2022-10-25 20:36:21 +0300

    added image sharing capability

--- Challenge - 1/Challenge - 1/DetailViewController.swift
+++ Challenge - 1/Challenge - 1/DetailViewController.swift
@@ -16,6 +16,8 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -26,6 +28,17 @@ class DetailViewController: UIViewController {
         
         navigationController?.navigationBar.prefersLargeTitles = false
         
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
