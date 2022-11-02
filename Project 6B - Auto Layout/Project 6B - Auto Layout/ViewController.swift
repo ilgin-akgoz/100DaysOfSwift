344d0aefcc31ac8243601a59504f3755c207f220
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2022-11-02 21:18:58 +0300

    day 31 challenges

--- Project 6B - Auto Layout/Project 6B - Auto Layout/ViewController.swift
+++ Project 6B - Auto Layout/Project 6B - Auto Layout/ViewController.swift
@@ -51,7 +51,7 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        //let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         
         // H: constraints are horizontal
         //for label in viewsDictionary.keys {
@@ -62,8 +62,16 @@ class ViewController: UIViewController {
         //view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
         
         for label in [label1, label2, label3, label4, label5] {
-            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            
+            //label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            
             label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
             
             if let previous = previous {
                 //we have a previous label - create a height constraint
