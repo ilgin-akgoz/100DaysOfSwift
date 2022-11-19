46d17aad639a8feb61c2da70aeacdcda4af3d61c
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2022-11-19 22:46:48 +0300

    project setup

---
+++ Milestone Challenge - 3/Milestone Challenge - 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone Challenge - 3
+//
+//  Created by Ilgın Akgöz on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
