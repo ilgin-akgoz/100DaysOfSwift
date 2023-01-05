818055da92e5be88ac4f4163c314212bd90079e9
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2023-01-05 23:20:24 +0300

    initial project setup

---
+++ Project 16 - Capital Cities/Project 16 - Capital Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 16 - Capital Cities
+//
+//  Created by Ilgın Akgöz on 5.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
