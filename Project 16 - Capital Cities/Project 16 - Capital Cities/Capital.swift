244047ce220649b45dadc690f30da10a10c1400a
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2023-01-05 23:43:47 +0300

    map annotations

---
+++ Project 16 - Capital Cities/Project 16 - Capital Cities/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project 16 - Capital Cities
+//
+//  Created by Ilgın Akgöz on 5.01.2023.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
